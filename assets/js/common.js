5603248ff7869337381fe5e7a7c1d1f9d3a450e8
dworker <dworker863@gmail.com>
2022-02-12 09:13:55 +0600

    Slider-specifications all screens

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,5 @@
 $(window).on('load', function () {
-  $('.owl-carousel').owlCarousel({
+  $('.carousel-view').owlCarousel({
     items: 2,
     loop: true,
     margin: 60,
@@ -7,6 +7,21 @@ $(window).on('load', function () {
     responsive: {
       1024: {
         items: 3,
+      },
+    },
+  });
+
+  $('.carousel-specifications').owlCarousel({
+    items: 2,
+    loop: true,
+    margin: 60,
+    autoWidth: true,
+    responsive: {
+      1024: {
+        items: 3,
+      },
+      1280: {
+        items: 4,
       },
     },
   });
